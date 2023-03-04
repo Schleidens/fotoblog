16852af55437e7e95cc9dba3ceebe67fda803ef0
Schleidens <alectineschleidens2016@gmail.com>
2023-03-04 13:51:35 -0500

    add login requirement for home-page and set redirect link for user trying to access home_page withiut logged in

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+#redirect page for guest users
+LOGIN_URL = 'login-page'
