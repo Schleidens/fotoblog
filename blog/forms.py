4c497b1f1c625f0cce823959a774a7d708550db4
Schleidens <alectineschleidens2016@gmail.com>
2023-04-02 23:43:50 -0400

    create form for blog card

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
-from blog.models import Photo
+from blog.models import Photo, Blog
 
 class photoForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ['image', 'caption']
+
+
+class blogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'content']
