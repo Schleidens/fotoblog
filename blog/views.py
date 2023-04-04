c10dc07a1ed13c4bd010b56f88385291185fc7b0
Schleidens <alectineschleidens2016@gmail.com>
2023-04-03 23:55:23 -0400

    create views to upload blog and photo at the same time

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from blog.forms import photoForm
+from blog.forms import photoForm, blogForm
 from blog.models import Photo
 
 # Create your views here.
@@ -56,3 +56,37 @@ def photo_upload(request):
 
     return render(request, 'photo_upload.html', context={'form': form})
 ''' 
+
+
+class blog_and_photo_upload(LoginRequiredMixin, View):
+    photo_form = photoForm
+    blog_form = blogForm
+    template = 'blog_view.html'
+    
+    def get(self, request):
+        context = {
+            'photo_form' : self.photo_form(),
+            'blog_form' : self.blog_form()
+        }
+        return render(request, self.template, context=context)
+
+    def post(self, request):
+        photo_form = self.photo_form(request.POST, request.FILES)
+        blog_form = self.blog_form(request.POST)
+        if all([photo_form.is_valid(), blog_form.is_valid()]):
+            photo = photo_form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+
+            return redirect('home-page')
+
+        context = {
+            'photo_form' : photo_form,
+            'blog_form' : blog_form
+        }
+
+        return render(request, self.template, context=context)
